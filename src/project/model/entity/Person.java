7eda170c83f99f40bc8c96ccc3846541c51efc00
Messbah.A <Messbah.A@Gmail.com>
2024-02-21 17:59:49 +0330

    First Upload

---
+++ src/project/model/entity/Person.java
@@ -0,0 +1,24 @@
+package project.model.entity;
+
+import com.google.gson.Gson;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+
+
+public class Person{
+    private int id;
+    private String name;
+
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+}
